71d776058cdad8e9ed2685df9c519002defcc2e2
MieczyslawMilej <mieczyslaw.milej@gmail.com>
2020-04-11 18:40:42 +0200

    Ukończenie kalkulatora dziesiętnego

--- js/DecCalculator.js
+++ js/DecCalculator.js
@@ -7,75 +7,42 @@ class DecCalculator extends Calculator {
   }
 
   add(numberX, numberY) {
-    // Dodaj numberX i numberY - tablice cyfr w taki sposób, aby zwrócić tablice o rozmiarze o 1 większym
-    // (wynik ma 1 cyfrę więcej). Dodawanie ma być wykonane w systemie liczbowym o podstawie 10.
-    // Zadeklaruj zmienną - wynik. Wynik będzie tablicą cyfr (dla zapisu cyfr użyjemy wartości liczbowych).
-    // Wynik będzie miał jedną cyfrę więcej niż dodawane wartości.
-    // Zadeklaruj zmienną reprezentującą wartość przenoszoną na poprzednie cyfry (np. dodając 8 i 4 przenosimy 1).
-    // Na samym starcie, gdy będziemy dodawali ostatnie cyfry, nic nie przenieśliśmy, więc na starcie wartość
-    // tej zmiennej będzie równa 0.
-    // Przebiegnij w pętli przez indeksy wszystkich cyfry jednej z dodawanych liczb, począwszy od cyfry ostatniej
-    // (dodajemy w tabelce zaczynając od dodawania cyfr jedności, czyli od ostatnich).
-    // W pętli dla każdej dodawanej pary cyfr, wylicz ich sumę powiększoną o przenoszoną wartość.
-    // Reszta z dzielenia tej sumy przez 10 będzie cyfrą wyniku,
-    // a wynik dzielenia tej sumy przez 10 (zaokrąglony do dołu - Math.floor) będzie przenoszoną wartością.
-    // Pierwzsą cyfrą wyniku będzie ostatnia przenoszona wartość.
-    // Zwróć tak przygotowany wynik.
     let result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
     for (let i = numberX.length - 1; i >= 0; i--) {
       let carry = 0;
-      if ((numberX[i] + numberY[i]) < 10) {
-        carry = Math.floor(numberX[i] + numberY[i] + result[i]);
-
+      let resulo = 0;
+      debugger;
+      if (numberX[i] + numberY[i] >= 10) {
+        carry = Math.floor((numberX[i] + numberY[i] + carry) / 10);
+        resulo = Math.floor((numberX[i] + numberY[i] + result[i]) % 10);
       } else {
-        carry = Math.floor((numberX[i] + numberY[i]) / 10);
+        result[i] = Math.floor(numberX[i] + numberY[i] + result[i]);
       }
-console.log(carry);
+      console.log(carry);
       if (carry > 0) {
+        result[i] = resulo;
         result[i - 1] = carry;
-      } else {
-        result[i ] = numberX[i] + numberY[i];
       }
-      // console.log(result);
-
-      // if (carryBit % 10) {
-      //   result[i] = carryBit / 10;
-      //   result[i - 1] = 1;
-      // } else {
-      //   result[i] = carryBit;
-      // }
     }
     return result;
   }
 
   changeNumber(root) {
-    // Metoda jest wywoływana, gdy użytkownik kliknie cyfrę z dodawanej liczby. root będzie rodzicem klikniętego
-    // spana.
-    // Temu spanowi przypisz atrybut contenteditable o wartości "true".
-    // Wywołaj również jego metodę focus.
     let activeElement = root.querySelector(".active");
     activeElement.setAttribute("contenteditable", "true");
     activeElement.focus();
   }
 
   updateResult() {
-    // Z tablicy resultNumberArray przepisz cyfry do odpowiednich elementów dokumentu (kalkulatora).
-    // Przypomnienie - odwołanie do elementu zawierającego wszystkie elementy kalkulatora: this.$calculatorDOMElement
-
     let root = this.$calculatorDOMElement;
     this.resultNumberArray.reverse();
     let $resultNumber = root.querySelectorAll(".result-bit > span");
     for (let i = $resultNumber.length - 1; i >= 0; i--) {
-      // debugger;
       $resultNumber[i].innerText = this.resultNumberArray[i];
     }
   }
 
   initEvents() {
-    // Wywołaj oryginalną (nadpisywaną) implementację metody initEvents.
-    // Z elementów kalkulatora wybierz ten ze znaczkiem "+". Przypisz jemu obsługę zdarzenia polegającego
-    // na jego kliknięciu.
-    // Po jego kliknięciu powinny zostać wywołane metody kalkulatora - aktualizująca wartości liczb przypisanych do właściwości kalkulatora oraz aktualizacja wyliczonego wyniku.
     super.initEvents();
 
     this.$calculatorDOMElement.addEventListener("click", event => {
